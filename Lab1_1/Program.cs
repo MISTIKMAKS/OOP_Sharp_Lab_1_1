6df76a72a741fb85707322b2d6cb6a0505eb9797
MISTIKMAKS <mistikmaks80885@gmail.com>
2022-02-23 16:20:05 +0200

    first master commit

---
+++ Lab1_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0; // вхідний параметр
+            double y = 0; // результат обчислення виразу
+            double A = 0; // проміжний результат - функціонально стала частина виразу
+            double B = 0; // проміжний результат - функціонально змінна частина виразу
+            Console.Write("x = ");
+            x = Convert.ToDouble(Console.ReadLine());
+            A = (2 + x) / (x * x) + 1;
+            // спосіб 1: розгалуження в скороченій формі
+            if (x < 0){
+                B = Math.Pow(x, 3) - 2 * Math.Pow(x, 4);
+            }
+            if (x <= 0 && x >= 2) {
+                B = Math.Pow(Math.Abs(x) + Math.Exp(x), 3);
+            }
+            if (x > 2) {
+                B = 4 * Math.Cos((x * x) - 2);
+            }
+            y = A + B;
+            Console.WriteLine("");
+            Console.WriteLine($"1) y = {Math.Round(y, 2)}");
+            // спосіб 2: розгалуження в повній формі
+            if (x < 0) {
+                B = Math.Pow(x, 3) - 2 * Math.Pow(x, 4);
+            }
+            else if (x > 2) {
+                B = 4 * Math.Cos((x * x) - 2);
+            }
+            else {
+                B = Math.Pow(Math.Abs(x) + Math.Exp(x), 3);
+            }
+            y = A - B;
+            Console.WriteLine($"2) y = {Math.Round(y, 2)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lab1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Lab1_1")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1e62a4b6-efe3-4924-9875-34819e72765c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
